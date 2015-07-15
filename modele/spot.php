0930f78c4bdcabc25a673fa554f377af6af908b2
hodiqual <alexis.hodiquet@gmail.com>
2015-07-15 20:13:22 +0200

    Correction test

--- modele/spot.php
+++ modele/spot.php
@@ -101,11 +101,11 @@ class SpotsManager extends ManagerDB
 	
 	public function getAll()
 	{
-		$lacanauDataMock = ['nomSpot' => 'Lacanau',
+		$lacanauDataMock = array ('nomSpot' => 'Lacanau',
 				'photoSpot' => 'TODO',
 				'urlGoogleMap' => '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11284.48954965727!2d-1.1947349999999974!3d45.00213650000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4801ffc558a23645%3A0xc02b43d4e1f03569!2sLacanau+Oc%C3%A9an%2C+33680+Lacanau!5e0!3m2!1sfr!2sfr!4v1436976508811" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>',
 				'idFSI' => 5
-		] ;
+		) ;
 		
 		$spots = [];
 		$spots[] = new Spot($lacanauDataMock);
