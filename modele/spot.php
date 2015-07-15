e1c31ffde68231ae7e836553acf15bc90e621be3
hodiqual <alexis.hodiquet@gmail.com>
2015-07-15 19:21:18 +0200

    Test Spot 2

--- modele/spot.php
+++ modele/spot.php
@@ -81,13 +81,13 @@ class SpotsManager extends ManagerDB
 {
 	
 	
-	/*private $dieppeDataMock = ['nomSpot' => 'Dieppe',
+	private $dieppeDataMock = array ('nomSpot' => 'Dieppe',
  								'photoSpot' => 'TODO',
  								'urlGoogleMap' => '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20550.94480663189!2d1.0834894999999998!3d49.92005045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e0a207670619f9%3A0xaea20a3d78418545!2sDieppe!5e0!3m2!1sfr!2sfr!4v1436979569652" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>',
  								'idFSI' => 1
-								] ;
+								) ;
 	
-	private $siouvilleDataMock = ['nomSpot' => 'Siouville',
+	/*private $siouvilleDataMock = ['nomSpot' => 'Siouville',
  								'photoSpot' => 'TODO',
  								'urlGoogleMap' => '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10351.957742046245!2d-1.8303565500000003!3d49.560222499999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480cec4db3fab077%3A0x72b1686bcb8dd123!2s50340+Siouville-Hague!5e0!3m2!1sfr!2sfr!4v1436980143468" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>',
  								'idFSI' => 2
@@ -97,7 +97,7 @@ class SpotsManager extends ManagerDB
  								'photoSpot' => 'TODO',
  								'urlGoogleMap' => '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10712.521514064989!2d-4.353274000000001!3d47.837067499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48173acc8773429b%3A0x64ed67bf332b3077!2sPointe+de+la+Torche%2C+29120+Plomeur!5e0!3m2!1sfr!2sfr!4v1436980210903" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>',
  								'idFSI' => 3
-								] ;*/
+								] ; */
 	
 	public function getAll()
 	{
