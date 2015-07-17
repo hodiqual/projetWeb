7ede0c935c2b63fa2113dcedfb8fbee41e1c7fac
thomasraimana <thomasraimana@hotmail.fr>
2015-07-17 16:52:07 +0200

    Update gestion_bdd.php

--- bdd/gestion_bdd.php
+++ bdd/gestion_bdd.php
@@ -58,7 +58,7 @@ function setMembre() {
 	$dbh = null;							// déconnexion de la bdd					
 }
 
-// Pour modifier un membre (repéré par son noMem caché)
+// Pour modifier un membre (repéré par son noMem)
 function updateMembre() {
 	$dbh = connectDb(); 					// connexion à la bdd
 	$sql = "UPDATE Membre SET nom=:nom, prenom=:prenom, avatar=:avatar, email=:email, mdp=:mdp WHERE noMem=:noMem;";
@@ -68,23 +68,24 @@ function updateMembre() {
 	$sth->bindParam(":avatar", $_REQUEST["avatar"], PDO::PARAM_STR);
 	$sth->bindParam(":email", $_REQUEST["email"], PDO::PARAM_STR);
 	$sth->bindParam(":mdp", $_REQUEST["mdp"], PDO::PARAM_STR);
-	$sth->bindParam(":noMem", $_REQUEST["noMem"], PDO::PARAM_STR);
+	$sth->bindParam(":noMem", $_SESSION["noMem"], PDO::PARAM_STR);
 	if ($sth->execute() == 0)
 		print($dbh->errorInfo()); 		// affiche message d'érreur de la bdd 
 	$dbh = null; 							// déconnexion de la bdd
 }
 
-// Pour supprimer un membre (repéré par son noMem caché)
+// Pour supprimer un membre (repéré par son noMem)
 function deleteMembre() {
 	$dbh = connectDb(); 					// connexion à la bdd
-	$sql = "DELETE FROM Membre WHERE noMem=:noMem;";
+	$sql = "DELETE FROM Membre WHERE noMem = :noMem;";
 	$sth = $dbh->prepare($sql);
-	$sth->bindParam(":noMem", $_REQUEST["noMem"], PDO::PARAM_STR);
+	$sth->bindParam(":noMem", $_SESSION["noMem"], PDO::PARAM_STR);
 	if ($sth->execute() == 0)
 		print($dbh->errorInfo()); 		// affiche message d'érreur de la bdd 
 	$dbh = null; 							// déconnexion de la bdd
 }
 
+// Pour obtenir la liste des sessions de surf disponibles
 function getListeSessions() {
 	$dbh = connectDb(); 					// connexion à la bdd
 	$sql = "SELECT nomSpot, dateAller, dateRetour, heureDep FROM SessionSurf WHERE dateAller >= :dateAller;";
@@ -98,6 +99,7 @@ function getListeSessions() {
 	return $result;
 }	
 
+// Pour ajouter un véhicule
 function setVehicule() {
 	$dbh = connectDb(); 										// connexion à la bdd
 	$sql = "INSERT INTO Vehicule (nbrPlaces, nbrPlanches, marqueVeh, modeleVeh, couleurVeh, photoVeh) VALUES (:nbrPlaces, :nbrPlanches, :marqueVeh, :modeleVeh, :couleurVeh, :photoVeh);";
@@ -114,6 +116,18 @@ function setVehicule() {
 	$dbh = null;												// déconnexion de la bdd					
 }
 
+// Pour supprimer un véhicule
+function deleteVehicule($noVeh) {
+	$dbh = connectDb(); 					// connexion à la bdd
+	$sql = "DELETE FROM Vehicule WHERE noVeh = :noVeh;";
+	$sth = $dbh->prepare($sql);
+	$sth->bindParam(":noVeh", $noVeh, PDO::PARAM_STR);
+	if ($sth->execute() == 0)
+		print($dbh->errorInfo()); 		// affiche message d'érreur de la bdd 
+	$dbh = null; 							// déconnexion de la bdd
+}
+
+// Pour ajouter une classe Possede
 function setPossede($noVeh, $dbh) {
 	$sql = "INSERT INTO Possede VALUES (:noMem, :noVeh);";
 	$sth = $dbh->prepare($sql);
@@ -123,15 +137,7 @@ function setPossede($noVeh, $dbh) {
 		print($dbh->errorInfo()); 	// affiche message d'érreur de la bdd 				
 }
 
-function getPossede($noMem) {
-	$sql = "SELECT *noVeh FROM Possede WHERE noMem = :noMem;";
-	$sth = $dbh->prepare($sql);
-	$sth->bindParam(":noMem", $_SESSION["noMem"], PDO::PARAM_STR);
-	$sth->bindParam(":noVeh", $noVeh, PDO::PARAM_STR);
-	if ($sth->execute() == 0)
-		print($dbh->errorInfo()); 	// affiche message d'érreur de la bdd 				
-}
-
+// Pour ajouter une session de surf
 function setSessionSurf() {
 	$dbh = connectDb(); 				// connexion à la bdd
 	$sql = "INSERT INTO SessionSurf (nomSpot, dateAller, dateRetour, lieuDep) VALUES (:nomSpot, :dateAller, :dateRetour, :lieuDep);";
@@ -151,6 +157,18 @@ function setSessionSurf() {
 	setParticipe($noSes);			// celui qui propose une session y participe					
 }
 
+// Pour supprimer une sessionSurf
+function deleteSessionSurf($noSes) {
+	$dbh = connectDb(); 					// connexion à la bdd
+	$sql = "DELETE FROM SessionSurf WHERE noSes = :noSes;";
+	$sth = $dbh->prepare($sql);
+	$sth->bindParam(":noSes", $noSes, PDO::PARAM_STR);
+	if ($sth->execute() == 0)
+		print($dbh->errorInfo()); 		// affiche message d'érreur de la bdd 
+	$dbh = null; 							// déconnexion de la bdd
+}
+
+// Pour ajouter une classe Propose
 function setPropose($noSes, $dbh) {
 	$sql = "INSERT INTO Propose VALUES (:noMem, :noSes);";
 	$sth = $dbh->prepare($sql);
@@ -160,12 +178,39 @@ function setPropose($noSes, $dbh) {
 		print($dbh->errorInfo()); 	// affiche message d'érreur de la bdd 				
 }
 
+// Pour ajouter une classe Participe
 function setParticipe($noSes) {
-	$dbh = connectDb();
+	$dbh = connectDb();				// connexion à la bdd
 	$sql = "INSERT INTO Propose VALUES (:noMem, :noSes);";
 	$sth = $dbh->prepare($sql);
 	$sth->bindParam(":noMem", $_SESSION["noMem"], PDO::PARAM_STR);
 	$sth->bindParam(":noSes", $noSes, PDO::PARAM_STR);
+	if ($sth->execute() == 0)
+		print($dbh->errorInfo()); 	// affiche message d'érreur de la bdd 
+	$dbh = null;						// déconnexion de la bdd			
+}
+
+// Pour supprimer une participation
+function deleteParticipe($noSes) {
+	$dbh = connectDb(); 					// connexion à la bdd
+	$sql = "DELETE FROM Participe WHERE noMem = :noMem AND noSes = :noSes;";
+	$sth = $dbh->prepare($sql);
+	$sth->bindParam(":noMem", $_SESSION["noMem"], PDO::PARAM_STR);
+	$sth->bindParam(":noSes", $noSes, PDO::PARAM_STR);
+	if ($sth->execute() == 0)
+		print($dbh->errorInfo()); 		// affiche message d'érreur de la bdd 
+	$dbh = null; 							// déconnexion de la bdd
+}
+
+// Pour ajouter un vehiculeSessionSurf
+function setVehiculeSessionSurf($noVeh, $noSes) {
+	$dbh = connectDb();				// connexion à la bdd
+	$sql = "INSERT INTO VehiculeSessionSurf VALUES (:noVeh, :noSes, :nbrPlacesDispo, :nbrPlanchesDispo);";
+	$sth = $dbh->prepare($sql);
+	$sth->bindParam(":noVeh", $noVeh, PDO::PARAM_STR);
+	$sth->bindParam(":noSes", $noSes, PDO::PARAM_STR);
+	$sth->bindParam(":nbrPlacesDispo", $_REQUEST["nbrPlacesDispo"], PDO::PARAM_STR);
+	$sth->bindParam(":nbrPlanchesDispo", $_REQUEST["nbrPlanchesDispo"], PDO::PARAM_STR);
 	if ($sth->execute() == 0)
 		print($dbh->errorInfo()); 	// affiche message d'érreur de la bdd 
 	$dbh = null;						// déconnexion de la bdd			
