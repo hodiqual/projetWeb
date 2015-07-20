2ca81c7d11bb73ceea484b647d738cd6f073aa84
thomasraimana <thomasraimana@hotmail.fr>
2015-07-21 00:32:13 +0200

    Update sql.php

--- sql.php
+++ sql.php
@@ -16,7 +16,10 @@ class SQL {
 class CreationBdd {
 	public static function main() {
 		$con = mysqli_connect("127.0.0.1","shippable","","test");
-    	$sql = file_get_contents("creation_bdd_clasNsurf.sql");
+		$sql = CREATE USER 'iessa2'@'localhost' IDENTIFIED BY '***';GRANT ALL PRIVILEGES ON *.* TO 'iessa2'@'localhost'
+		IDENTIFIED BY '***' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0
+		MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;GRANT ALL PRIVILEGES ON `iessa2\_%`.* TO 'iessa2'@'localhost';
+    	$sql .= file_get_contents("creation_bdd_clasNsurf.sql");
     	$array = explode(";\n", $sql);
     	$b = true;
     	for ($i=0 ; $i < count($array) ; $i++) {
