903bc654c2c697265e9130e68aba4c4dbcaee4cb
hodiqual <alexis.hodiquet@gmail.com>
2015-07-21 10:33:02 +0200

    test ajout

--- test.php
+++ test.php
@@ -30,6 +30,37 @@ class CreationBddTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals("Raimana", $membre->prenom());
 	}
 	
+	/**
+	 * @depends testCreationBdd
+	 */
+	public function testMembresManager() {
+		$membresManager = new MembresManager(null);
+		$membre = $membresManager->authentifier("thomasr@gmail.com", "iessa");
+		$this->assertEquals("THOMAS", $membre->nom());
+		$this->assertEquals("Raimana", $membre->prenom());
+	}
+	
+
+	
+	private $donneeNouvelleInscription  = array ( 'nom' => 'Wayne',
+												  'prenom' => 'Bruce',
+												  'email' => 'batman@wayne.enac',
+												  'mdp' => 'toto') ;
+	/**
+	 * @depends testCreationBdd
+	 */
+	public function testNouvelleInscription() {
+		$membre = new Membre($donneeNouvelleInscription);
+		$membresManager = new MembresManager(null);
+		$membresManager->sauvegarder($membre);
+		$this->assertNotEquals(0, $membre->noMem());
+		
+		$membreReload = $membresManager->authentifier($donneeNouvelleInscription['email'], $donneeNouvelleInscription['toto']);
+		$this->assertEquals($membreReload->noMem(), $membre->noMem());
+		$this->assertEquals($membreReload->nom(), $membre->nom());
+	
+	}
+	
 }
 
 ?>
