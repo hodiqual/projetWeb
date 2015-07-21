096d742d5a881bc83a4799bac4c32de76ec6661a
hodiqual <alexis.hodiquet@gmail.com>
2015-07-21 08:29:03 +0200

    prout

--- sql.php
+++ sql.php
@@ -16,8 +16,8 @@ class SQL {
 class CreationBdd {
 	public static function main() {
 		$con = mysqli_connect("127.0.0.1","shippable","","test");
-		$sql = "CREATE USER 'iessa'@'localhost' IDENTIFIED BY '***';GRANT ALL PRIVILEGES ON *.* TO 'iessa'@'localhost'
-		IDENTIFIED BY '***' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0
+		$sql = "CREATE USER 'iessa'@'localhost' IDENTIFIED BY 'iessa';GRANT ALL PRIVILEGES ON *.* TO 'iessa'@'localhost'
+		IDENTIFIED BY 'iessa' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0
 		MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;GRANT ALL PRIVILEGES ON `iessa\_%`.* TO 'iessa'@'localhost';";
     		mysqli_query($con, $sql);
     		$sql = file_get_contents("creation_bdd_clasNsurf.sql");
