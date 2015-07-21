fee572838d627a03f1f2e4b00ca1398bd0796657
thomasraimana <thomasraimana@hotmail.fr>
2015-07-21 02:59:20 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
 require_once("sql.php");
 require_once("modele/SessionSurf.php");
+require_once("modele/Membre.php");
 
 
 class CreationBddTest extends PHPUnit_Framework_TestCase {
@@ -17,6 +18,16 @@ class CreationBddTest extends PHPUnit_Framework_TestCase {
 		$sessionSurfsManager = new SessionSurfsManager(null);
 		$doc = $sessionSurfsManager->getAll();
 		$this->assertEquals(3, count($doc));
+	}
+	
+	/**
+     * @depends testCreationBdd
+     */
+	public function testMembresManager() {
+		$membresManager = new MembresManager(null);
+		$membre = $membresManager->authentifier("thomasr@gmail.com", "iessa");
+		$this->assertEquals("THOMAS", $membre->nom());
+		$this->assertEquals("Raimana", $membre->prenom());
 	}
 	
 }
