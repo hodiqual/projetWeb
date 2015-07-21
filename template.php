1f91bb10ca285910a2ac005fbab72cc7887880fc
hodiqual <alexis.hodiquet@gmail.com>
2015-07-21 04:03:57 +0200

    Enleve inutile

--- template.php
+++ template.php
@@ -102,70 +102,11 @@ function ecrireFooter()
 {
 	?>
 
-	<!-- Twitter Feed -->
-	<div id="twitter-feed" class="page-alternate">
-		<div class="container">
-			<div class="row">
-				<div class="span12">
-					<div class="follow">
-						<a href="https://twitter.com/Bluxart" title="Follow Me on Twitter"
-							target="_blank"><i class="font-icon-social-twitter"></i></a>
-					</div>
-
-					<div id="ticker" class="query"></div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- End Twitter Feed -->
-
-	<!-- Socialize -->
-	<div id="social-area" class="page">
-		<div class="container">
-			<div class="row">
-				<div class="span12">
-					<nav id="social">
-						<ul>
-							<li><a href="https://twitter.com/Bluxart"
-								title="Follow Me on Twitter" target="_blank"><span
-									class="font-icon-social-twitter"></span></a></li>
-							<li><a href="http://dribbble.com/Bluxart"
-								title="Follow Me on Dribbble" target="_blank"><span
-									class="font-icon-social-dribbble"></span></a></li>
-							<li><a href="http://forrst.com/people/Bluxart"
-								title="Follow Me on Forrst" target="_blank"><span
-									class="font-icon-social-forrst"></span></a></li>
-							<li><a href="http://www.behance.net/alessioatzeni"
-								title="Follow Me on Behance" target="_blank"><span
-									class="font-icon-social-behance"></span></a></li>
-							<li><a href="https://www.facebook.com/Bluxart"
-								title="Follow Me on Facebook" target="_blank"><span
-									class="font-icon-social-facebook"></span></a></li>
-							<li><a href="https://plus.google.com/105500420878314068694"
-								title="Follow Me on Google Plus" target="_blank"><span
-									class="font-icon-social-google-plus"></span></a></li>
-							<li><a href="http://www.linkedin.com/in/alessioatzeni"
-								title="Follow Me on LinkedIn" target="_blank"><span
-									class="font-icon-social-linkedin"></span></a></li>
-							<li><a href="http://themeforest.net/user/Bluxart"
-								title="Follow Me on ThemeForest" target="_blank"><span
-									class="font-icon-social-envato"></span></a></li>
-							<li><a href="http://zerply.com/Bluxart/public"
-								title="Follow Me on Zerply" target="_blank"><span
-									class="font-icon-social-zerply"></span></a></li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- End Socialize -->
-
 	<!-- Footer -->
 	<footer>
 		<p class="credits">
-		IESSA14 - <a href="#CV" title="Brushed | Responsive One Page Template">HODIQUET Alexis</a>
-			et <a href="#CV" title="Alessio Atzeni | Web Designer &amp; Front-end Developer">THOMAS Raimana</a>
+		IESSA14 - <a href="#CV" title="Co Gliss">HODIQUET Alexis</a>
+			et <a href="#CV" title="Co Gliss">THOMAS Raimana</a>
 		</p>
 	</footer>
 	<!-- End Footer -->
