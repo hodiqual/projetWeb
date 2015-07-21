5c08170b0f1cc6df3d3ec5f8cafccc2718732230
thomasraimana <thomasraimana@hotmail.fr>
2015-07-21 02:01:49 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,29 +1,22 @@
 <?php
 require_once("sql.php");
 
-class SQLTest extends PHPUnit_Framework_TestCase {
-  public function testSQL() {
-    $sql = new SQL();
-    $doc = $sql->main();
-    $this->assertNotEquals($doc, false);
-  }
-}
-
-/*class SpotTest extends PHPUnit_Framework_TestCase {
-	public function testGetAllSpots() {
-		$spotsManager = new SpotsManager(NULL);
-		
-		$spotsList = $spotsManager->getAll();
-		$this->assertEquals(8, count($spotsList));
-	}
-}*/
-
 class CreationBddTest extends PHPUnit_Framework_TestCase {
 	public function testCreationBdd() {
 		$creationBdd = new CreationBdd();
 		$doc = $creationBdd->main();
 		$this->assertNotEquals($doc, false);
 	}
+	
+	/**
+     * @depends testCreationBdd
+     */
+	public function testSessionSurfsManager() {
+		$sessionSurfsManager = new SessionSurfsManager(null);
+		$doc = $sessionSurfsManager->getAll();
+		$this->assertEquals(3, count($doc));
+	}
+	
 }
 
 ?>
